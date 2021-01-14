0b1bfade0b1026c77a522d0cb76a0a67eadb090e
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-01-14 21:25:28 +0530

    serialzing for the custom user authentication

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'api',
     'api.category',
-    'api.product',
+    'api.product'
 ]
 
 MIDDLEWARE = [
@@ -129,6 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+#AUTH_USER_MODEL="user.CustomUser"
 
 CORS_ALLOW_ALL_ORIGINS=True
 
